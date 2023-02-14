1a28b2be122a01ab131431f0da96e7ba3854bf1f
Matheus Veiga <matheus.veiga@pravaler.com.br>
2023-02-14 18:29:56 -0300

    Adicionado documentação swagger

--- src/application/dtos/customer.dto.ts
+++ src/application/dtos/customer.dto.ts
@@ -1,10 +1,20 @@
-export type DocumentDTO = {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class DocumentDTO {
+  @ApiProperty()
   number: string;
+
+  @ApiProperty()
   type: string;
-};
+}
 
 export class CustomerDTO {
+  @ApiProperty()
   id: string;
+
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   document: DocumentDTO;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { RootModule } from './root.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Customers API')
+    .setDescription('The customers API')
+    .setVersion('1.0')
+    .addTag('customers')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
